aeb7c040ae2ed37da99ebfc4bed66793ab62612a
Dereemii <leshermira@gmail.com>
2023-03-23 20:37:34 -0300

    added chat route

---
+++ src/client/Home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <div id="firebaseui-auth-container"></div>
+    </>
+  );
+};
+
+export default Home;
--- src/client/main.tsx
+++ src/client/main.tsx
@@ -1,14 +1,26 @@
 import React from "react";
-import "./firebase"
-import { createRoot } from 'react-dom/client';
-import "./firebase"
+import "./firebase";
+import { createRoot } from "react-dom/client";
+import "./firebase";
+
+import Chat from "./Chat"
+import Home from "./Home"
 
 
-const rootElement = document.getElementById('app')
-if(!rootElement) throw new Error("Failed to find root element")
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+  },
+  {
+    path: "/chat",
+    element: <Chat/>,
+  }
+]);
+
+const rootElement = document.getElementById("app");
+if (!rootElement) throw new Error("Failed to find root element");
 const root = createRoot(rootElement);
-root.render(<h1>Hello, world</h1>);
-
-
-
-
+root.render(<RouterProvider router={router} />);
